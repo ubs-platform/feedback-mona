2a2278c2e518c6d438de08b587d1182cedc9f2f9
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2024-09-25 22:18:25 +0300

    main

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     transport: Transport.TCP,
     options: {
       port: INTERNAL_COMMUNICATION.port,
-      host: INTERNAL_COMMUNICATION.host,
+      host: '0.0.0.0',
     },
   });
   app.setGlobalPrefix(globalPrefix);
