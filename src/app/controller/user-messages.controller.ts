848c2a1f26af58fe23a43e3c56a9fe1498d67786
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2024-09-25 20:03:18 +0300

    Version upgraded to 3.0.11

--- src/app/controller/user-messages.controller.ts
+++ src/app/controller/user-messages.controller.ts
@@ -1,10 +1,10 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import { Body, Controller, Inject, Param, Post } from '@nestjs/common';
 import { BaseCrudControllerGenerator } from './base/base-crud.controller';
 import { UserMessageModel } from '../model/user-message.model';
 import { IUserMessageDto, IUserMessageSearch } from 'libs/common/src/lib/dto';
 import { ControllerConfiguration } from './base/controller-configuration';
 import { UserMessageService } from '../service/user-message.service';
-import { MessagePattern } from '@nestjs/microservices';
+import { ClientKafka, MessagePattern } from '@nestjs/microservices';
 const config: ControllerConfiguration = {
   authorization: {
     ADD: { needsAuthenticated: false },
@@ -19,8 +19,17 @@ export class UserMessageController extends BaseCrudControllerGenerator<
   IUserMessageDto,
   IUserMessageSearch
 >(config) {
-  constructor(private userMsgService: UserMessageService) {
+  constructor(
+    private userMsgService: UserMessageService,
+    @Inject('KAFKA_CLIENT') private kafkaClient: ClientKafka
+  ) {
     super(userMsgService);
+
+    this.kafkaClient.emit('register-category', {
+      category: 'USER_MESSAGE',
+      serviceTcpHost: process.env['U_USERS_MONA_INTERNAL_COM_HOST'],
+      serviceTcpPort: process.env['U_USERS_MONA_INTERNAL_COM_PORT'],
+    });
   }
 
   @Post(':id/resolve')
